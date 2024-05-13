cef152895a213c9a209643e4478e8722be4e4286
msti <contact@mstillwell.dev>
2024-05-13 17:24:13 -0500

    note: removed stuff, added stuff

---
+++ css.sh
@@ -0,0 +1,6 @@
+#! /usr/bin/fish
+
+# NOTE: currently zellij has issues running npm from layout so using this
+
+cd assets
+npm run css-watch
--- justfile
+++
@@ -1,5 +0,0 @@
-css:
-	cd assets/ && npm run css
-
-run: css
-	cargo watch -x check -x test -x 'shuttle run'
